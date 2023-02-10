57ae831acdb61e773152742139d8f5c9be708c1a
Kayleigh <57508868+gordokay@users.noreply.github.com>
2023-02-10 10:30:47 -0500

    Add player turns

--- src/game.js
+++ src/game.js
@@ -6,4 +6,9 @@ export default function init(p1 = "Player 1", p2 = "Player 2", isPlayingComputer
   Event.emit('ship placement', player1.gameboard.shipGrid);
   const player2 = new Player(p2, isPlayingComputer);
   Event.subscribe(player1, "attack", player1.attack, [player2.gameboard]);
+  let currentPlayer = "player1";
+  while(!player1.gameboard.allSunk() && !player2.gameboard.allSunk()) {
+    Event.emit('change turn', currentPlayer);
+    currentPlayer = 'player2';
+  }
 }
